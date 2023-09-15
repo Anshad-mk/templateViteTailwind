96635caa0346f5ea89150a6f03a49c8cc56fa7d7
Anshad.MK <84131708+Anshad-mk@users.noreply.github.com>
2023-09-16 01:44:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+  
+  return (
+    <>
+    <div>
+      <h1 className='text-2xl'>Template</h1>
+      <h1 className=''>Template</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
